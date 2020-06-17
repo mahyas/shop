fb95233ae9a73439428a120a8f2c2957d61c4db5
mahyas <mahy.sadeghi@gmail.com>
2020-06-17 15:41:49 -0700

    added router for shop page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-
-const FlatsPage = () => (
-  <div>
-    <h1>FLATS PAGE</h1> 
-  </div>
-
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/flats' component={FlatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
