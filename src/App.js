b076f911761f48f6a79506c6f8aeab50922d3b61
mahyas <mahy.sadeghi@gmail.com>
2020-06-25 22:09:17 -0700

    Added manual signup

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user =>{
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot(snapshot => {
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signin from '../../components/signin/signin.component';
+import Signup from '../../components/signup/signup.component';
 import './signin-signup.styles.scss';
 
 const SigninSignupPage = () => (
     <div className = 'signin-signup'> 
         <Signin />
+        <Signup />
     </div>
 )
 
