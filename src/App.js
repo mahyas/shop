c8a9a22cae86610089018ac54992cfbc875c4faf
mahyas <mahy.sadeghi@gmail.com>
2020-07-08 13:16:16 -0700

    Redirect to homepage when user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -21,12 +21,13 @@ class App extends React.Component {
           setCurrentUser({
             id: snapshot.id,
             ...snapshot.data()
-          })
+          });
         });
       }
-      setCurrentUser({userAuth});
+      setCurrentUser(userAuth);
       });
   }
+
 
   componentWillUnmount(){
     this.unsubscribeFromAuth();
@@ -39,14 +40,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SigninSignupPage} />
+          <Route 
+            exact
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ?(
+                <Redirect to='/' />
+              ): (
+               <SigninSignupPage />
+              )
+            }
+            />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) =>  ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
