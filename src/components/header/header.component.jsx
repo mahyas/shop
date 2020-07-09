a02e6f1dee892a1d058822fcafb606d083c8f2a7
mahyas <mahy.sadeghi@gmail.com>
2020-07-08 20:15:23 -0700

    added cart drop down functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+
+const Header = ({currentUser, hidden}) => (
     <div className = 'header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -32,12 +34,14 @@ const Header = ({currentUser}) => (
                 )}
                 <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+  });
 
 export default connect(mapStateToProps)(Header);
 
