69964a59cb16ea1e62c91638737d5c1bcc03b191
mahyas <mahy.sadeghi@gmail.com>
2020-07-14 16:33:02 -0700

    added stripe and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H4wOqHMDNraMl41jcqBUzwE61on8h2AP4Z7Jf8kU1uuAqUkaXb06EDxjr6T9hKI7UEYdX7kiCnCoxhmDv099ZXW00ZpwMRsFG';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
